4ea6f1bc4c316fded1defabc4189a26b1a3e33a8
unknown <elhamhashemi56@yahoo.com>
2023-08-14 17:35:40 +0200

    events and methods

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const app = Vue.createApp({
     // data , function
-    
+
     // template:"<h2>hello word</h2>"
     data(){
       return{
@@ -8,7 +8,17 @@ const app = Vue.createApp({
         author:"sadegh hedayat",
         age:45
       }
+    },
+    methods:{
+      changeTitle(abc){
+        // console.log("clicked");
+        this.title=abc
+      }
     }
+
+
+
+
   });
   
   app.mount("#app");
